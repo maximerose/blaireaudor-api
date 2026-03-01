23d30957cdc0c425eaed6fd7045cf7f46e532598
Maxime <maxime.r80@gmail.com>
2026-03-01 11:50:53 +0100

    style: application des standards PSR-12 via PHP-CS-Fixer

--- back/src/Kernel.php
+++ back/src/Kernel.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 namespace App;
-use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
