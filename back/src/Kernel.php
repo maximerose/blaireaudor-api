4c1a64912599cd4ba2f7a0409188a3f70f11a878
Maxime <maxime.r80@gmail.com>
2026-03-01 11:46:30 +0100

    Test de l'automatisation

--- back/src/Kernel.php
+++ back/src/Kernel.php
@@ -1,14 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
-
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 }
-
-
-
