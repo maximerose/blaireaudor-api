6ad217d98c4eafcc5d587982824c7613fe08a7a0
Maxime <maxime.r80@gmail.com>
2026-05-30 12:37:28 +0200

    ops: inclure le conteneur proxy dans le script de déploiement automatique

--- docker/webhook/deploy.sh
+++ docker/webhook/deploy.sh
@@ -5,5 +5,5 @@ cd /volume1/docker/blaireaudor
 git config --global --add safe.directory /volume1/docker/blaireaudor
 
 git pull origin main
-docker compose -f compose.prod.yaml up -d --build back front
+docker compose -f compose.prod.yaml up -d --build back front proxy
 echo "✅ Déploiement terminé avec succès !"
